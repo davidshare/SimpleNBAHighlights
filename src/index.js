ac34ed1ac8d24264f53f57104e8832b2d6866d6b
David Essien <davidessienshare@gmail.com>
2018-05-30 04:42:52 +0100

    create Home component and define it in index file

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
+//Components
+import Home from './components/home'
+
 ReactDOM.render(<App />, document.getElementById('root'));
