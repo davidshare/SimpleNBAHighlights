37f66d70f6101ec1ca324e120b1f29ab51d0cec9
David Essien <davidessienshare@gmail.com>
2018-05-30 05:12:54 +0100

    add logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,14 @@ const Header = () =>{
 	
 	return(
 		<header>
-			<div>Header</div>
+			<div className="flex-container">
+				<Link to="/" className="logo">
+					<span></span>
+				</Link>
+				<nav>
+					<Link to="/teams">Teams</Link>
+				</nav>
+			</div>
 		</header>
 	)
 }
