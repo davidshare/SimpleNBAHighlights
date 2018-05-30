bfed9a6adc507e476c45fb67113351da762a689a
David Essien <davidessienshare@gmail.com>
2018-05-30 04:50:11 +0100

    create components for header and footer and define them in index

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () =>{
+	
+	return(
+		<h3>Home page</h3>
+	)
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import App from './components/app';
 
 //Components
 import Home from './components/home'
+import Header from './components/header'
+import Footer from './components/footer'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
