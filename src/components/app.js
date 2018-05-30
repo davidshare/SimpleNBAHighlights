714bd5a4e9e004d7c2aa35f3fe86e301da138e09
David Essien <davidessienshare@gmail.com>
2018-05-30 05:01:59 +0100

    move call to components from index.js to app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 
+//Components
+import Home from './home'
+import Header from './header'
+import Footer from './footer'
+
 class App extends Component{
 	render(){
 		return (
--- src/index.js
+++ src/index.js
@@ -2,10 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-//Components
-import Home from './components/home'
-import Header from './components/header'
-import Footer from './components/footer'
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
