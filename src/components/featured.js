beea9e5500716fc59931b6959e6d5de0060dc816
David Essien <davidessienshare@gmail.com>
2018-06-01 02:52:49 +0100

    fix bug in Featured component

--- src/components/featured.js
+++ src/components/featured.js
@@ -1,42 +1,41 @@
-import React from 'react'
-import Slider from 'react-slick'
-
+import React from 'react';
+import Slider from 'react-slick';
 
 const settings = {
-	arrows:true,
-	dots:true,
-	infinite:true,
-	speed:500,
-	slidesToShow:1,
-	slidesToScroll:1
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+};
+
+const generateSlides = ({slides}) => {
+    if(slides){
+         return (
+            <Slider {...settings}>
+                {slides.map(function(item){
+                    return (
+                        <div key={item.id} className="item_slider" 
+                            style={{background:`url(/images/covers/${item.cover})`}}>
+                            <div className="caption">
+                                <h4>{item.topic}</h4>
+                                <p>{item.title}</p>
+                            </div>
+                        </div>
+                        )  
+                })}
+            </Slider>
+        )
+    }
 }
 
-const generateSlides = ({slides}) =>{
-	if(slides){
-		return (
-			<Slider {...settings}>
-				{slides.map((item)=>{
-					return(
-						<div key={item.id} className="item-slider" style={{background:`url(/images/covers/${item.cover})`}}>
-							
-							<div className="caption">
-								<h4>{item.topic}</h4>
-								<p>{item.title}</p>
-							</div>
-						</div>
-					)
-				})}
-			</Slider>
-		)
-	}
+const Featured = (props) => {
+    return(
+        <div>
+            {generateSlides(props)}
+        </div>
+    )
 }
 
-const Featured =(props) =>{
-	return (
-		<div>
-			{generateSlides(props)}
-		</div>
-	)
-}
-
-export default Featured
+export default Featured;
